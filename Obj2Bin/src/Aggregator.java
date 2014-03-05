034064ce3c52db670fcb019bbac1dfa393854204
Grayver <grayver@gmail.com>
2014-03-06 02:44:25 +0400

    Load key mesh

--- Obj2Bin/src/Aggregator.java
+++ Obj2Bin/src/Aggregator.java
@@ -10,6 +10,9 @@ public class Aggregator {
 	private static final int mSectorCount = 8;
 	private static final String[] mCirclePrefixes = new String[] { "i", "m", "o" };
 	
+	private static final int mKeyCount = 4;
+	private static final String mKeyPrefix = "h";
+	
 	public static void main(String[] args) {
 		try {
 			DataOutputStream dos = new DataOutputStream(new FileOutputStream(String.format("out/mesh_%dx%d.bin", mCircleCount, mSectorCount)));
@@ -33,6 +36,23 @@ public class Aggregator {
 			System.err.println(e.getMessage());
 		}
 
+		try {
+			DataOutputStream dos = new DataOutputStream(new FileOutputStream(String.format("out/mesh_key.bin", mKeyCount)));
+			dos.writeInt(mKeyCount);
+			
+			for (int i = 0; i < mKeyCount; i++) {
+				FileInputStream is = new FileInputStream(String.format("out/mesh_%s%d.bin", mKeyPrefix, i + 1));
+				byte[] buffer = new byte[is.available()];
+				is.read(buffer, 0, buffer.length);
+				dos.write(buffer);
+				is.close();
+			}
+			
+			dos.flush();
+			dos.close();
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		}
 	}
 
 }
