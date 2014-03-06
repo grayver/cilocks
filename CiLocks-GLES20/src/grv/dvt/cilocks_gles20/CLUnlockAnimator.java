89150a32e91fd70dc095be20db39e2bfb57f659f
Grayver <grayver@gmail.com>
2014-03-07 00:55:06 +0400

    Unlock animator

---
+++ CiLocks-GLES20/src/grv/dvt/cilocks_gles20/CLUnlockAnimator.java
@@ -0,0 +1,42 @@
+package grv.dvt.cilocks_gles20;
+
+public class CLUnlockAnimator extends Animator {
+
+	private CLLock mCircleLock;
+	
+	private float mTargetDistance;
+	
+	public CLUnlockAnimator(long duration, CLLock circleLock, float targetDistance) {
+		super(duration);
+		mCircleLock = circleLock;
+		
+		mTargetDistance = targetDistance;
+		
+		synchronized (mCircleLock) {
+			CLKeyCircle keyCircle = mCircleLock.getKeyCircle();
+			for (int i = 0; i < keyCircle.getSectorCount(); i++)
+				keyCircle.getSector(i).setDistance(0.0f);
+		}
+	}
+	
+	@Override
+	protected void onAnimationUpdate() {
+		float distance = mFraction * mTargetDistance;
+		
+		synchronized (mCircleLock) {
+			CLKeyCircle keyCircle = mCircleLock.getKeyCircle();
+			for (int i = 0; i < keyCircle.getSectorCount(); i++)
+				keyCircle.getSector(i).setDistance(distance);
+		}
+	}
+
+	@Override
+	protected void onAnimationEnd() {
+		synchronized (mCircleLock) {
+			CLKeyCircle keyCircle = mCircleLock.getKeyCircle();
+			for (int i = 0; i < keyCircle.getSectorCount(); i++)
+				keyCircle.getSector(i).setDistance(mTargetDistance);
+		}
+	}
+
+}
